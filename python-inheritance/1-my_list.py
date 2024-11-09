2c7f653d79f5da2ad402a16ccd21de53406672ca
Dnshutikelly <dushimimanansutikelly@gmail.com>
2024-11-09 09:43:53 -0800

    kelly

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,7 +5,7 @@ A class that inherits from list and adds a method to print the list sorted.
 
 class MyList(list):
     """Class MyList that extends list and adds a print_sorted method."""
-    
+
     def __str__(self):
         """Return the string representation of the list."""
         return super().__str__()
