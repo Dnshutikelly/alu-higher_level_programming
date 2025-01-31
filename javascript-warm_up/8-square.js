79f3b4a975235a1b321ab9b81c56fb9fda1932be
Dnshutikelly <dushimimananshutkelly@gmail.com>
2025-01-31 13:06:30 -0800

    hello

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -4,10 +4,10 @@ if (isNaN(size)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
-    let kelly = '';
+    let gisa = '';
     for (let z = 0; z < size; z++) {
-      kelly += 'X';
+      gisa += 'X';
     }
-    console.log(kelly);
+    console.log(gisa);
   }
 }
